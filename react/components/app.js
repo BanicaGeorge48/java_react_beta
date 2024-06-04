5207cfb837a6049f23a3ccbf1fd4e216fd095262
GEO <146426440+BanicaGeorge48@users.noreply.github.com>
2024-06-04 16:13:25 +0300

    Add files via upload

--- react/components/app.js
+++ react/components/app.js
@@ -1,6 +1,7 @@
 import { Component } from '../lib/react.js'
 import User from './user.js'
 import Wrapper from './wrapper.js'
+import UserStyled from './user-styled.js'
 
 class App extends Component {
   render() {
@@ -8,8 +9,12 @@ class App extends Component {
       <div class="app">
         ${new Wrapper({
           children: `
-            <h2>React.js ⭐️⭐️⭐️⭐️⭐️</h2>
+            <h1>React.js ⭐️⭐️⭐️⭐️⭐️</h1>
             ${new User({
+              name: 'Ash',
+              avatar: './images/ash.jpg'
+            }).render()}
+            ${new UserStyled({
               name: 'Ash',
               avatar: './images/ash.jpg'
             }).render()}
